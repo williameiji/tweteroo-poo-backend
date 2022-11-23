67d0ab5ba0304e7cd24df7fbc7cc50d8b129bbf7
williameiji <william.fujimori@gmail.com>
2022-11-23 13:30:48 -0300

    feat: route to create a tweet

--- src/router/tweetsRouter.js
+++ src/router/tweetsRouter.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
+import { TweetsController } from "../controller/tweetsContoller.js";
 
 const tweetsRouter = Router();
+
+tweetsRouter
+	.post("/tweets", (req, res) => {
+		const { tweet, username } = req.body;
+
+		const data = new TweetsController().createTweet(tweet, username);
+
+		res.status(data.status).send(data.message);
+	})
+	.get("/tweets")
+	.get("/tweets/:username");
 
 export { tweetsRouter };
