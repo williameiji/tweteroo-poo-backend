4469d3fd305724663646e1a5d4450c013c807b24
williameiji <william.fujimori@gmail.com>
2022-11-23 13:59:52 -0300

    feat: add controller to get tweets

--- src/controller/tweetsContoller.js
+++ src/controller/tweetsContoller.js
@@ -4,4 +4,12 @@ export class TweetsController {
 	createTweet(tweet, username) {
 		return new TweetsServices().createTweet(tweet, username);
 	}
+
+	getTweetsByOrder(page) {
+		return new TweetsServices().getTweetsByOrder(page);
+	}
+
+	getTweetsByName(username) {
+		return new TweetsServices().getTweetsByName(username);
+	}
 }
