8ea2966dd84c19a532460e2748f52bf01a584169
williameiji <william.fujimori@gmail.com>
2022-11-23 14:01:03 -0300

    feat: get tweets by limit or by name

--- src/services/tweetsServices.js
+++ src/services/tweetsServices.js
@@ -26,6 +26,27 @@ export class TweetsServices {
 
 		return { status: 200, message: "OK, seu tweet foi criado" };
 	}
+
+	getTweetsByOrder(page) {
+		if (page && page < 1) {
+			return { status: 400, message: "Informe uma página válida!" };
+		}
+		const limite = 10;
+		const start = (page - 1) * limite;
+		const end = page * limite;
+
+		if (tweets.length <= 10) {
+			return { status: 200, message: [...tweets].reverse() };
+		}
+
+		return { status: 200, message: [...tweets].reverse().slice(start, end) };
+	}
+
+	getTweetsByName(username) {
+		const tweetsDoUsuario = tweets.filter((t) => t.username === username);
+
+		return { status: 200, message: tweetsDoUsuario };
+	}
 }
 
 export const tweets = [];
