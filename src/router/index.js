e8cf65f687bf33beee4265a521b96ece02bb169e
williameiji <william.fujimori@gmail.com>
2022-11-23 13:15:15 -0300

    feat: add users route

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { tweetsRouter } from "./tweetsRouter.js";
+import { usersRouter } from "./usersRouter.js";
 
 const router = Router();
 
-router.use(tweetsRouter);
+router.use(tweetsRouter).use(usersRouter);
 
 export default router;
